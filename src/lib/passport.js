f4f704831f4920dedbdad2e56a2e5eaa818ffe44
Daniel220903 <dvillalobos878@gmail.com>
2023-04-12 20:56:20 -0600

    Actualizado....

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -3,6 +3,8 @@ const passport  = require('passport');
 const LocalStrategy  = require('passport-local').Strategy;
 const pool = require('../database');
 const helpers = require ('./helpers'); 
+// const validator = require("email-validator"); ESTA MADRE NO SIRVE
+var mbv = require("mailboxvalidator-nodejs"); mbv.MailboxValidator_init("JE9R6BYHJD01HGA1P2BR");
 
 passport.use('local.signin' , new LocalStrategy({
     usernameField: 'username',
@@ -42,35 +44,57 @@ passport.use('local.signup', new LocalStrategy({
     passReqToCallback: true
 }/*Esta funcion es un callback,se ejecuta hasta que se acabe el local strategy*/ 
     ,async (req, username, password, done) => {
-        const{fullname} = req.body;
+        const{fullname, email} = req.body;
         const newUser= {
             username: username,
+            email: email,
             password: password,
             fullname: fullname
         };
-        const special = /[!@#$%^&*(),.?":{}|<>]/;
-        if((special.test(newUser.fullname)) || (special.test(newUser.password)) || (special.test(newUser.username))){
-            done(null, false, req.flash('message','NO SPECIAL CHARACTERS ACCEPTED'));
-        }else{
-            const poolUsername = await pool.query('SELECT username FROM users WHERE username = ?',  newUser.username);
-            if(poolUsername[0]){
-                done(null, false, req.flash('message','USERNAME ALREADY EXISTS'));
+   
+            const special = /[!#$%^&*(),?"{}|<>]/;
+            if((special.test(newUser.fullname)) || (special.test(newUser.password)) || (special.test(newUser.username)) || (special.test(newUser.email)) ){
+                done(null, false, req.flash('message','NO SPECIAL CHARACTERS ACCEPTED'));
             }else{
-                const Capittal = /[A-Z]/;
-                const Lower = /[a-z]/;
-                const Number = /\d/;
+                mbv.MailboxValidator_single_query(email, mbv_read_single);
+                
+               async function mbv_read_single(err, res, data) {
+                // console.log("email_address: " + data.email_address);
+		        // console.log("domain: " + data.domain);
+		        // console.log("is_free: " + data.is_free);
+		        // console.log("is_syntax: " + data.is_syntax);
+		        // console.log("is_domain: " + data.is_domain);
+		        // console.log("is_smtp: " + data.is_smtp);
+		        // console.log("is_verified: " + data.is_verified);
+                const exist = data.is_verified;//ESTA MADRE REGRESA UN STRING EN LUGAR DE UN BOOLEAN
+                    if(exist == 'True'){
+                        const poolUsername = await pool.query('SELECT username FROM users WHERE username = ?',  newUser.username);
+                        if(poolUsername[0]){
+                            done(null, false, req.flash('message','USERNAME ALREADY EXISTS'));
+                        }else{
+                            const Capittal = /[A-Z]/;
+                            const Lower = /[a-z]/;
+                            const Number = /\d/;
+                           
+                                if ( (!Capittal.test(newUser.password)) || (!Number.test(newUser.password)) || (!Lower.test(newUser.password)) ) {
+                                    done(null, false, req.flash('message','PASSWORD MUST HAVE AT LEAST ONE UPPERCASE AND LOWERCASE LETTER, AND ONE NUMBER'));
+                                }else{       
+                                            newUser.password = await helpers.encryptPassword(password);
+                                            const result = await pool.query('INSERT INTO users SET ?', [newUser]);
+                                            newUser.id = result.insertId;
+                                            // console.log(result);
+                                            return done(null, newUser);
+                                }   
+                        }
+                    } else{
+                        done(null, false, req.flash('message','THERES ANY TROUBLE WITH YOUR EMAIL '));
+                    }         
+                
+                }
                
-                    if ( (!Capittal.test(newUser.password)) || (!Number.test(newUser.password)) || (!Lower.test(newUser.password)) ) {
-                        done(null, false, req.flash('message','PASSWORD MUST HAVE AT LEAST ONE UPPERCASE AND LOWERCASE LETTER, AND ONE NUMBER'));
-                    }else{       
-                                newUser.password = await helpers.encryptPassword(password);
-                                const result = await pool.query('INSERT INTO users SET ?', [newUser]);
-                                newUser.id = result.insertId;
-                                // console.log(result);
-                                return done(null, newUser);
-                    }   
             }
-        }
+      
+        
 }));
 
 
